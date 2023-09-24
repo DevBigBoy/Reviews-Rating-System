b9b3fe469b866b5bcd9babf10b060a8608c57d46
Mohamed Elshazly <56097354+DevMohamedElshazly@users.noreply.github.com>
2023-09-24 12:50:13 +0300

    add header

--- all_posts.php
+++ all_posts.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
 
-    <!-- Custom CSS -->
+    <!--=================== Custom CSS=================== -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
@@ -19,20 +19,9 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <!--================= HEADER Start =================-->
+    <?php  include 'components/header.php' ;?>
+    <!--================= HEADER Start =================-->
 
 
 
---
+++ components/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <section class="flex">
+        <a href="all_posts.php" class="logo">
+            Shezo
+        </a>
+
+        <nav class="navbar">
+            <a href="all_posts.php" class="fa-regular fa-eye"></a>
+            <a href="login.php" class="fa-solid fa-arrow-right-to-bracket"></a>
+            <a href="register.php" class="fa-regular fa-registered"></a>
+        </nav>
+    </section>
+
+</header>
